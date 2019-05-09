616bff857d7a0d5c98d2479728d9552fd089b180
Daniel <nhanxtrand@gmail.com>
2019-05-09 02:15:50 +0000

    Add logic for user log in

--- express_server.js
+++ express_server.js
@@ -88,6 +88,11 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/urls');
+});
+
 
 
 function generateRandomString() {
